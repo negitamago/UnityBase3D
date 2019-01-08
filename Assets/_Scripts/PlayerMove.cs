d100ddaaf89fef6185cd275b1c7f698f12aa9ce8
negilab <negilabworks@gmail.com>
2019-01-08 19:29:47 +0900

    基本パック追加（地面、プレイヤー、マテリアル、プレイヤーとカメラを動かすスクリプト）

---
+++ Assets/_Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    private Rigidbody rb;
+
+    private float inputH, inputV;
+
+    private void Start ()
+    {
+        rb = GetComponent<Rigidbody> ();
+    }
+
+    private void Update ()
+    {
+        inputH = Input.GetAxis ( "Horizontal" );
+        inputV = Input.GetAxis ( "Vertical" );
+    }
+
+    private void FixedUpdate ()
+    {
+        var moveValue = new Vector3 ( inputH, 0f, inputV ) * speed * Time.deltaTime;
+        rb.MovePosition ( rb.position + moveValue );
+    }
+}
---
+++ Assets/_Scripts/TargetFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private Vector3 offset;
+
+    private void Start ()
+    {
+        offset = this.transform.position - target.position;
+    }
+
+    private void LateUpdate ()
+    {
+        this.transform.position = target.position + offset;
+    }
+}
