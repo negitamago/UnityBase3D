35b0af39310005c8f809d5028d1a103fac477c44
negilab <negilabworks@gmail.com>
2019-01-10 12:28:32 +0900

    add Pickups

---
+++ Assets/UnityBase3D/Scripts/DestroyByContact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace negitama.UnityBase3D
+{
+    public class DestroyByContact : MonoBehaviour
+    {
+        [SerializeField] string otherTag;
+        [SerializeField] float lifeTime;
+
+        private void OnTriggerEnter ( Collider other )
+        {
+            if ( otherTag != string.Empty )
+            {
+                if ( !other.CompareTag ( otherTag ) ) return;
+            }
+
+            Destroy ( this.gameObject, lifeTime );
+        }
+    }
+}
---
+++ Assets/UnityBase3D/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace negitama.UnityBase3D
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] Vector3 rotateSpeed;
+
+        private Rigidbody rb;
+
+        private void Start () => rb = GetComponent<Rigidbody> ();
+
+        private void FixedUpdate ()
+        {
+            var rot = Quaternion.Euler(rotateSpeed * Time.time);
+            rb.MoveRotation ( rot );
+        }
+    }
+}
