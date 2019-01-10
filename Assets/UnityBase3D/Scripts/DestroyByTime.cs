b938fcd4d788079afe3314e85f0aa0b91eff78e2
negilab <negilabworks@gmail.com>
2019-01-10 14:21:48 +0900

    ScoreText 追加

---
+++ Assets/UnityBase3D/Scripts/DestroyByTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace negitama.UnityBase3D
+{
+    public class DestroyByTime : MonoBehaviour
+    {
+        [SerializeField] float lifeTime;
+
+        private void Start () => Destroy ( this.gameObject, lifeTime );
+    }
+}
